9fce156125c26ce8fa8094d8410e603a89a68f8f
michaelhodgkins <mhodgkins1997@gmail.com>
2023-04-03 17:55:32 +0100

    New input system player movement

--- Project/Assets/Scripts/PlayerMovement.cs
+++ Project/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    public float speed = 1;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Test");
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed, ForceMode.Impulse);
     }
 }
